95dc610715f658e7591e8e3c3b22496c9e3f6fdd
angcyo <angcyo@126.com>
2019-05-23 19:34:59 +0800

    update

--- app/src/main/java/com/angcyo/rqrcodedemo/RCode.java
+++ app/src/main/java/com/angcyo/rqrcodedemo/RCode.java
@@ -123,8 +123,6 @@ public class RCode {
             canvas.drawBitmap(src, 0, 0, null);
             canvas.scale(scaleFactor, scaleFactor, srcWidth / 2, srcHeight / 2);
             canvas.drawBitmap(logo, (srcWidth - logoWidth) / 2, (srcHeight - logoHeight) / 2, null);
-            canvas.save(Canvas.ALL_SAVE_FLAG);
-            canvas.restore();
         } catch (Exception e) {
             bitmap = null;
         }
